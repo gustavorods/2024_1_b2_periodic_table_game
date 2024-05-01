cb4c70159ec07442918ff2e6cde3c012c89efd33
gustavorods <gustavo.rlsilva07@gmail.com>
2024-05-01 20:53:58 -0300

    feat script JS: Function to check the user's response has been launched and is already showing in the html

--- js/script.js
+++ js/script.js
@@ -1,17 +1,46 @@
 // HTML 
 let element_icon_html = document.querySelector("#element_icon");
 let next_element_html = document.querySelector("#next_element");
+let textField = document.querySelector("#user_response");
+let submitBotton = document.querySelector("#submit_button");
 
 // taking the path of each image
 let iron = "./images/ferro.png";
 let gold = "./images/Ouro.png";
 let plutonium = "./images/Plutonio.png";
 
-// Making array withe the ways
-var elements = [iron, gold, plutonium];
+// Making object with the variables
+var elements = {
+    Ferro: iron,
+    Ouro: gold,
+    Plutônio: plutonium
+  };
 
 // function to choose an element randomly
+let randomElementKey; // I will use it to check with the user's response
+
 function randomElement() {
-    let randomNumber = Math.floor(Math.random() * elements.length);
-    element_icon_html.setAttribute('src',elements[randomNumber]);   
+    textField.style.border = "2px solid gray";
+
+    let elementKeys = Object.keys(elements);
+    let randomIndex = Math.floor(Math.random() * elementKeys.length);
+    randomElementKey = elementKeys[randomIndex];
+    let randomElementImagePath = elements[randomElementKey];
+
+    element_icon_html.setAttribute('src', randomElementImagePath);
+}
+
+// Function to check the user's response
+function checkUserResponde(userResponse) {
+    if(userResponse == randomElementKey) {
+        textField.style.border = "2px solid green";
+
+        // Aguarda 5000 milissegundos (5 segundos) antes de executar o restante do código
+        setTimeout(function () {
+            randomElement();
+            textField.value = "";
+        }, 1000);
+    } else {
+        textField.style.border = "2px solid red";
+    }
 }
--- js/user_interections.js
+++ js/user_interections.js
@@ -1,2 +1,5 @@
 next_element_html.addEventListener('click', randomElement);
 
+submitBotton.addEventListener('click', () => {
+    checkUserResponde(textField.value);
+})
